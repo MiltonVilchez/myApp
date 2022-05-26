9e80b5b9d4cff79557e43579af53cbd361aadc34
MiltonVilchez <vilchezmilton8@gmail.com>
2022-05-25 19:19:30 -0600

    Creando componentes sin estilos

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import LoginForm from './src/components/LoginForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world! I'm starting in mobile development ❤️</Text>
+      <LoginForm></LoginForm>
     </View>
   );
 }
